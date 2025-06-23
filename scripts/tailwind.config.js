0ca37dae21428faf197fa13c6448311c15eeb972
sandleon <andrew.scullion@pm.me>
2025-06-23 18:33:10 +0100

    added tailwind css and changed background colours

---
+++ scripts/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'custom-black': '#020300',
+                'custom-olive': '#8E8152',
+                'custom-walnut': '#59544B',
+                'custom-cool-gray': '#7D8CA3',
+                'custom-carolina-blue': '#79A9D1',
+            }
+        }
+    }
+}; 
